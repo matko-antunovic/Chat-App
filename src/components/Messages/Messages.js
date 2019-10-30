fc3ac4c96f1066264657ebb8be92c8e5394745be
matko-antunovic <47244433+matko-antunovic@users.noreply.github.com>
2019-10-30 11:40:43 +0100

    adding pannels,chat ui funct.

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,8 +1,22 @@
 import React from "react";
+import MessagesHeader from "./MessagesHeader"
+import MessageForm from "./MessageForm"
+import {Segment,Comment} from "semantic-ui-react"
 
 class Messages extends React.Component {
   render() {
-    return <div>Messages</div>;
+    return (
+      <React.Fragment>
+        <MessagesHeader/>
+        <Segment>
+          <Comment.Group className="messages">
+            
+          </Comment.Group>
+        </Segment>
+
+        <MessageForm/> 
+      </React.Fragment>
+    );
   }
 }
 
